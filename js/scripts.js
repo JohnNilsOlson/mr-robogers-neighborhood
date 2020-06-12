1ff749354430dfdaead4a1242c1188847c9a8235
JohnNils Olson <johnnils@gmail.com>
2020-06-12 10:30:37 -0700

    add script to filter inputs with instances of 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 function robo(numInput) {
-  if (isNaN(numInput) === true) {
+  numInt = parseInt(numInput);
+  numSplit = numInput.split("");
+  numArray = [];
+    
+  if (isNaN(numInt) === true) {
     return alert("Please enter a number!");
+  } else if ((numSplit.indexOf("1") >= 0)) {
+    return alert("Beep!")  
   } else {
-    numArray = [];
-    while (numInput >= 0) {
-      (numArray).push(numInput);
-      numInput --;
+    while (numInt >= 0) {
+      (numArray).push(numInt);
+      numInt --;
     }
     return alert((numArray).reverse());
   }
@@ -14,7 +19,7 @@ function robo(numInput) {
 $(document).ready(function() {
   $("#number").submit(function(event) {
     event.preventDefault;
-    let numInput = parseInt($("input#num-input").val());
+    let numInput = $("input#num-input").val();
     robo(numInput);
   });
 });
