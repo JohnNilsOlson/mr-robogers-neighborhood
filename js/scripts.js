3c23cbcb9185c56cabcb41da3b747322e6c57604
JohnNils Olson <johnnils@gmail.com>
2020-06-12 09:14:05 -0700

    add function to script to test input for NaN

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+function robo(numInput) {
+  if (isNaN(numInput) === true) {
+    return alert("Please enter a number!");
+  } else {
+    return alert(numInput);
+  }
+}
+
 $(document).ready(function() {
   $("#number").submit(function(event) {
     event.preventDefault;
     let numInput = parseInt($("input#num-input").val());
-    alert(numInput);
+    robo(numInput);
   });
 });
